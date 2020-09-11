24deb98070214e62c146d311dec094e670918d00
root <orck10@gmail.com>
2020-09-11 08:13:18 -0300

    swager e getById

--- sevicosDados/src/main/java/com/example/demo/controller/JogoController.java
+++ sevicosDados/src/main/java/com/example/demo/controller/JogoController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +40,15 @@ public class JogoController {
 	@GetMapping(path = "/controlador/ativos")
 	public ResponseEntity<List<Jogo>> getAllActivoByControlador(@RequestParam(value = "nome", defaultValue = "Teste", required = true) String nome){
 		return ResponseEntity.ok(jogoService.getByAticoControlador(nome));
+	}
+	
+	@GetMapping(path = "/jogar")
+	public ResponseEntity<Jogo> getById(@RequestParam(value = "id", defaultValue = " ", required = true) String id){
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Access-Control-Allow-Origin", "*");
+		headers.add("Access-Control-Allow-Methods", "GET");
+		headers.add("Access-Control-Allow-Headers", "*");
+		headers.add("Access-Control-Max-Age", "86400");
+		return ResponseEntity.ok().headers(headers).body(jogoService.getById(id));
 	}
 }
