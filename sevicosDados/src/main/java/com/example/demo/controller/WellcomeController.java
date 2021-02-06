766f2cba19f8ac57f4483c6f808b525811e707b7
root <root@jorrge-Inspiron-3576>
2021-02-05 23:59:38 -0300

    Ajuste do docker-compose

--- sevicosDados/src/main/java/com/example/demo/controller/WellcomeController.java
+++ sevicosDados/src/main/java/com/example/demo/controller/WellcomeController.java
@@ -1,17 +1,24 @@
 package com.example.demo.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WellcomeController {
+	
 	@RequestMapping("/")
 	public String welcome() {
+		Date date = new Date();
+		System.out.println(date.toString() + " - WellCome");
 		return "wellcome";
 	}
 	
 	@RequestMapping("/painel")
 	public String painel() {
+		Date date = new Date();
+		System.out.println(date.toString() + " - Painel");
 		return "PainelDados";
 	}
 }
